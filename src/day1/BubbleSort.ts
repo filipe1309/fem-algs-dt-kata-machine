05e480606b88fdcc448206b37d8c896db2ba425d
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-15 17:50:50 -0300

    feat(sort): implement Bubble Sort

--- src/day1/BubbleSort.ts
+++ src/day1/BubbleSort.ts
@@ -1,3 +1,15 @@
 export default function bubble_sort(arr: number[]): void {
+  return mySolution(arr);
+}
 
+function mySolution(arr: number[]): void {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j+1]) {
+        const tmp = arr[j];
+        arr[j] = arr[j+1]
+        arr[j+1] = tmp
+      }
+    }
+  }
 }
