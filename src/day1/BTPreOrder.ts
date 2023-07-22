0e963a736e7f4b9d4884cc50ba709b9d65d4b7e1
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-22 17:23:16 -0300

    feat(trees): add BTPreOrder my solution

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,12 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
+  let numbers: number[] = [];
+  return traverse(head, numbers)
+}
 
+function traverse(head: BinaryNode<number> | null, numbers: number[]): number[] {
+  if (!head) return numbers;
+  numbers.push(head.value);
+  traverse(head.left, numbers);
+  traverse(head.right, numbers);
+  return numbers;
 }
