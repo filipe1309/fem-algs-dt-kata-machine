fd83678683208a95890652179cf17c2e0867e883
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-07-22 17:36:15 -0300

    refactor(trees): BTPreOrder  solution

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,12 +1,20 @@
 export default function pre_order_search(head: BinaryNode<number>): number[] {
-  let numbers: number[] = [];
-  return traverse(head, numbers)
+  const path: number[] = []
+  traverse(head, path)
+  return path;
 }
 
-function traverse(head: BinaryNode<number> | null, numbers: number[]): number[] {
-  if (!head) return numbers;
+function traverse(head: BinaryNode<number> | null, numbers: number[]): void {
+  if (!head) return;
+
+  // recurse
+
+  // pre
   numbers.push(head.value);
+
+  // recurse
   traverse(head.left, numbers);
   traverse(head.right, numbers);
-  return numbers;
+
+  // post
 }
