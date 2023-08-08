170980b20e12a38fe82b45cc3e6c03d9ff63afa9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-08 19:50:00 -0300

    feat(tree-search): add my solution 2 to depth-first search

--- src/day1/DFSOnBST.ts
+++ src/day1/DFSOnBST.ts
@@ -1,13 +1,20 @@
 export default function dfs(head: BinaryNode<number>, needle: number): boolean {
-  return mySolution(head, needle);
+  return mySolution2(head, needle);
 }
 
 function mySolution(head: BinaryNode<number>, needle: number): boolean {
   if (head.value === needle) return true;
   if (head.left === null && head.right === null) return false;
-  
+
   const resultLeft = head.left ? dfs(head.left, needle) : false;
   const resultRight = head.right ? dfs(head.right, needle) : false;
 
   return resultLeft || resultRight;
+}
+
+function mySolution2(curr: BinaryNode<number> | null, needle: number): boolean {
+  if (!curr) return false;
+  if (curr.value === needle) return true;
+
+  return mySolution2(curr.left, needle) || mySolution2(curr.right, needle);
 }
