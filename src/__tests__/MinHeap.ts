210908eb96c40c7eefb57d0ac8046c083e54e1e9
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-10 19:08:32 -0300

    feat(heap): add my solution to heap insert

--- src/__tests__/MinHeap.ts
+++ src/__tests__/MinHeap.ts
@@ -1,30 +1,47 @@
 import MinHeap from "@code/MinHeap";
 
-test("min heap", function () {
-    const heap = new MinHeap();
+describe("min heap", () => {
+    test("insert", function () {
+        const heap = new MinHeap();
 
-    expect(heap.length).toEqual(0);
+        expect(heap.length).toEqual(0);
+        expect(heap.insert(5)).toEqual([5]);
+        expect(heap.length).toEqual(1);
+        expect(heap.insert(3)).toEqual([3, 5]);
+        expect(heap.insert(69)).toEqual([3, 5, 69]);
+        expect(heap.insert(420)).toEqual([3, 5, 69, 420]);
+        expect(heap.insert(4)).toEqual([3, 4, 69, 420, 5]);
+        expect(heap.insert(1)).toEqual([1, 4, 3, 420, 5, 69]);
+        expect(heap.insert(8)).toEqual([1, 4, 3, 420, 5, 69, 8]);
+        expect(heap.insert(7)).toEqual([1, 4, 3, 7, 5, 69, 8, 420]);
+        expect(heap.length).toEqual(8);
+    });
 
-    heap.insert(5);
-    heap.insert(3);
-    heap.insert(69);
-    heap.insert(420);
-    heap.insert(4);
-    heap.insert(1);
-    heap.insert(8);
-    heap.insert(7);
-
-    expect(heap.length).toEqual(8);
-    expect(heap.delete()).toEqual(1);
-    expect(heap.delete()).toEqual(3);
-    expect(heap.delete()).toEqual(4);
-    expect(heap.delete()).toEqual(5);
-    expect(heap.length).toEqual(4);
-    expect(heap.delete()).toEqual(7);
-    expect(heap.delete()).toEqual(8);
-    expect(heap.delete()).toEqual(69);
-    expect(heap.delete()).toEqual(420);
-    expect(heap.length).toEqual(0);
-});
-
+    test("delete", function () {
+        const heap = new MinHeap();
+    
+        expect(heap.length).toEqual(0);
+    
+        heap.insert(5);
+        heap.insert(3);
+        heap.insert(69);
+        heap.insert(420);
+        heap.insert(4);
+        heap.insert(1);
+        heap.insert(8);
+        heap.insert(7);
+    
+        expect(heap.length).toEqual(8);
+        expect(heap.delete()).toEqual(1);
+        expect(heap.delete()).toEqual(3);
+        expect(heap.delete()).toEqual(4);
+        expect(heap.delete()).toEqual(5);
+        expect(heap.length).toEqual(4);
+        expect(heap.delete()).toEqual(7);
+        expect(heap.delete()).toEqual(8);
+        expect(heap.delete()).toEqual(69);
+        expect(heap.delete()).toEqual(420);
+        expect(heap.length).toEqual(0);
+    });
+})
 
--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,43 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.data = [];
+        this.length = 0;
     }
 
-    insert(value: number): void {
-
-}
+    insert(value: number): number[] {
+        this.mySolutionInsert(value)
+        return this.data
+    }
     delete(): number {
+        return this.data.shift() || 0;
+    }
 
-}
+    mySolutionInsert(value: number) {
+        this.data.push(value);
+        this.length++;
+        this.mySolutionInsertHeapifyUp()
+    }
+
+    mySolutionInsertHeapifyUp(): void {
+        let parentIdx = this.parent(this.length - 1);
+        let parent = this.data[parentIdx];
+        let nodeIdx = this.length-1;
+        let node = this.data[nodeIdx];
+        while (node < parent) {
+            this.data[nodeIdx] = parent;
+            this.data[parentIdx] = node;
+
+            nodeIdx = parentIdx
+            parentIdx = Math.floor(((parentIdx - 1) - 1) / 2);
+            parent = this.data[parentIdx];
+            node = this.data[nodeIdx];
+        }
+    }
+
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
 }
