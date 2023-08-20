932d250f4c368ea5994cfdc7aeee3ba0d0d78cfe
Filipe Leuch Bonfim <filipe1309@gmail.com>
2023-08-20 17:19:52 -0300

    feat(graphs): add solution to DFS Graph with Adj Lists

--- src/day1/DFSGraphList.ts
+++ src/day1/DFSGraphList.ts
@@ -1,3 +1,37 @@
-export default function dfs(graph: WeightedAdjacencyList, source: number, needle: number): number[] | null {
+function walk(graph: WeightedAdjacencyList, 
+  curr: number, 
+  needle: number,
+  seen: Boolean[],
+  path: number[]): boolean {
+    if (seen[curr]) return false;
 
+    // recurse
+    //pre
+    seen[curr] = true;
+    path.push(curr);
+    if (curr === needle) { return true; }
+
+    //recurse
+    const list = graph[curr];
+    for (let i = 0; i < list.length; i++) {
+      const edge = list[i];
+      if (walk(graph, edge.to, needle, seen, path)) { 
+        return true;
+      }
+    }
+
+    //post
+    path.pop();
+
+    return false;
+  }
+
+// Complexity (wc): O(v + e) time, v=vertices,e=edges
+export default function dfs(
+  graph: WeightedAdjacencyList, 
+  source: number, 
+  needle: number): number[] | null {
+    let path: number[] = [];
+    walk(graph, source, needle, [], path);
+    return path.length ? path : null;
 }
